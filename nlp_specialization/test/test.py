647a4c2ed7451c73289340da187373db36cf4985
Sadra Avestan <44984349+avestamh@users.noreply.github.com>
2024-11-16 15:23:07 -0500

    Delete nlp_specialization/test directory

--- nlp_specialization/test/test.py
+++
@@ -1,6 +0,0 @@
-my_list = ['apple', 'banana', 'cherry', '', 'cherry']
-for i, item in enumerate(my_list):
-    print(f'index {i}: {item}' )
-
-lll= [i for i, item in enumerate(my_list) if item]
-print(lll)
